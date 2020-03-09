620199c82e89a144f54939cae20532c046d539af
Mikey Johnson <50218184+MikeySnowHydro@users.noreply.github.com>
2020-03-08 17:04:37 -0700

    Add files via upload

---
+++ lat_lon_to_UTM_10.R
@@ -0,0 +1,29 @@
+########################################################################################################
+### lat_long_to_UTM_10.r
+###
+### A simple function that will convert latitude and longitude to nothing and easing for zone UNM 10
+###
+### note: lonitude (E) is negitive
+###
+### By: Mikey Johnson
+########################################################################################################
+
+### Loading Libraries ###
+library(rgdal)
+
+### defining function ###
+lat_lon_to_UTM_10 <- function(latitude,longitude){
+cord.dec <- SpatialPoints(cbind(longitude, latitude), proj4string=CRS("+proj=longlat"))
+cord.UTM <- spTransform(cord.dec, CRS("+proj=utm +zone=10 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"))
+temp <- extent(cord.UTM)
+return.data <-c(temp@xmin,temp@ymax,10)
+return(return.data)
+}
+
+
+### function test, Eugene Airport: northing 4876320, easing 491987, UTM Zone 10 ###
+#lat_lon_to_UTM_10(44.039996,-123.1000122 )
+
+
+
+

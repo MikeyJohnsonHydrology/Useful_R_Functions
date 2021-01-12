a082a724b814023fa8e27ec625f790bb28ce9101
Mikey Johnson <50218184+MikeySnowHydro@users.noreply.github.com>
2021-01-12 13:41:18 -0800

    Add files via upload

--- Hogg Pass SWE.R
+++ Hogg Pass SWE.R
@@ -1,10 +1,11 @@
+###############################################################################################
 # Example script to collect and process SNOTEL date
-# Example site is Hogg Pass in the McKenzie River Basin
+# Example site is Hogg Pass SNOTEL
 #
 # Mikey Johnson
 # mikeyj@nevada.unr.edu
 #
-# Last Eddited: 2020-04-09
+# Last Eddited: 2021-01-12
 #
 ###############################################################################################
 # loading librarys
@@ -21,9 +22,10 @@ library(plotly)     # interactive plotting
 site_meta_data <- snotel_info() # loading all the SNOTEL station data
 
 Hogg_Pass <- snotel_download(site_id = 526, internal = TRUE) # downloading Hogg Pass, SWE[mm] and temp[Degrees C]
-#HP <- filter(Hogg_Pass, date <= "2019-09-30", date >= "2012-10-01") # seperating out the data (End Date, Start Date)
+#HP <- filter(Hogg_Pass, date <= "2018-09-30", date >= "2000-10-01") # seperating out the data (End Date, Start Date)
 #HP <- filter(Hogg_Pass, date <= "2015-09-30", date >= "2014-10-01") # Low Snow Year
-HP <- filter(Hogg_Pass, date <= "2014-09-30", date >= "2013-10-01") # Normal Snow Year
+HP <- filter(Hogg_Pass, date <= "2016-09-30", date >= "2015-10-01") # Medium Snow Year
+#HP <- filter(Hogg_Pass, date <= "2017-09-30", date >= "2016-10-01") # High Snow Year
 
 # Mt. Rose
 #Mt.Rose <- snotel_download(site_id = 652, internal = TRUE) # downloading Hogg Pass, SWE[mm] and temp[Degrees C]
@@ -39,6 +41,12 @@ ggplotly(ggplot()+
            theme_cowplot(12))
 
 
+
+###############################################################################################
+# Yearly Analysis
+# Note: If you use multiple years of data it will be summed over time
+
+
 ###############################################################################################
 # calcualting cumulative gain and loss of swe
 
@@ -46,7 +54,7 @@ HP <- HP %>% mutate(cumulative_gain=rep(0,nrow(HP)))
 HP$cumulative_gain[1] <- HP$snow_water_equivalent[1]
 for (i in 2:nrow(HP)){
 HP$cumulative_gain[i] <- ifelse(HP$snow_water_equivalent[i]>HP$snow_water_equivalent[i-1],
-                                HP$cumulative_gain[i-1]+HP$snow_water_equivalent[i],
+                                HP$cumulative_gain[i-1]+(HP$snow_water_equivalent[i]-HP$snow_water_equivalent[i-1]),
                                 HP$cumulative_gain[i-1])
 }
 
@@ -54,13 +62,21 @@ HP$cumulative_gain[i] <- ifelse(HP$snow_water_equivalent[i]>HP$snow_water_equiva
 HP <- HP %>% mutate(cumulative_loss=rep(0,nrow(HP)))
 for (i in 2:nrow(HP)){
   HP$cumulative_loss[i] <- ifelse(HP$snow_water_equivalent[i]<HP$snow_water_equivalent[i-1],
-                                  HP$cumulative_loss[i-1]-HP$snow_water_equivalent[i],
+                                  HP$cumulative_loss[i-1]-(HP$snow_water_equivalent[i-1]-HP$snow_water_equivalent[i]),
                                   HP$cumulative_loss[i-1])
 }
 
+ggplotly(ggplot(data = HP)+
+           geom_line(aes(x=date,y=cumulative_gain)) +
+           geom_line(aes(x=date,y=cumulative_loss)) +
+           geom_hline(yintercept = 0, linetype = "dotted") +
+           xlab("") + ylab("Snow Water Equivalant (mm)")+
+           ggtitle("Cunulative Loss and Gain")+
+           theme_cowplot(12))
+
 
 ###############################################################################################
-# flagging days where cumulative snowfall is >3[cm] or >30[mm]
+# flagging days where snowfall is >3[cm] or >30[mm]
 
 HP <- HP %>% 
   mutate(del_swe = (c(0,diff(snow_water_equivalent)))) %>%
@@ -68,10 +84,26 @@ HP <- HP %>%
 
 ggplotly(ggplot(data = HP)+
            geom_point(aes(x=date,y=Storm_Flag)) +
-           xlab("") + ylab("Snow Water Equivalant (mm)")+
+           xlab("") + ylab("")+
            ggtitle("Storm Days")+
            theme_cowplot(12))
 
+
+###############################################################################################
+# flagging days where snowmelt is >1[cm] or >10[mm] Note: I might change this threshold
+# Note: I need to find a way to seperate snowmelt from sublimation
+
+# Note: Commented out for now
+#
+#HP <- HP %>%
+#  mutate(Melt_Flag = ifelse(del_swe<=1,0,1))
+#
+#ggplotly(ggplot(data = HP)+
+#           geom_point(aes(x=date,y=Melt_Flag)) +
+#           xlab("") + ylab("")+
+#           ggtitle("Melt Days")+
+#           theme_cowplot(12))
+#
 
 ###############################################################################################
 # Counting Storms, # consider time step threshhold for gap between storms

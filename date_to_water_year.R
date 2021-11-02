ce9a1013fda129902e642afb07045e7d25c5a801
Mikey Johnson <50218184+MikeyJohnsonHydrology@users.noreply.github.com>
2021-11-02 10:19:19 -0700

    Add files via upload

---
+++ date_to_water_year.R
@@ -0,0 +1,18 @@
+#############################################################
+### date_to_water_year
+###
+### A simpel script to convert a date object to a water year
+###
+#############################################################
+
+date_to_water_year <- function(date){
+  year = as.numeric(format(date, "%Y"))
+  month = as.numeric(format(date, "%m"))
+  water_year <- ifelse(month < 10, year, year + 1)
+  return(water_year)
+  }
+
+# Testing the function
+#date_to_water_year(as.Date("2020-02-11"))
+#date_to_water_year(as.Date("2020-09-10"))
+#date_to_water_year(as.Date("2020-10-10")) 
